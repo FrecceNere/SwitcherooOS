40ca39662ec4d9286ede335ebe2e21d01bbdf973
Raffa <raffaelegual@tutamail.com>
2025-07-16 14:57:07 +0200

    Added OS detection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import re
+import platform
 import gui.gui
 import gui.windows_ui
 import gui.image_rc
@@ -9,36 +10,36 @@ from gui.gui import MainApp
 from PySide6.QtWidgets import QApplication
 
 def is_live():
-    out = subprocess.check_output("df ~", shell=True).decode()
-    full = out.strip().split("\n")[-1].split()[0]
-    fs_device = full.split("/")[-1]  # ex. "loop0", "sda1", "nvme0n1p2"
+    out = subprocess.run("df ~", shell=True, capture_output=True, text=True, check=False)
+    fs = out.stdout.strip().split("\n")[-1].split()[0].split("/")[-1]
 
-    parts = subprocess.check_output("cat /proc/partitions", shell=True).decode().split()
+    parts = subprocess.run("awk '{print $4}' /proc/partitions", shell=True, capture_output=True, text=True)
+    devices = parts.stdout.split()
 
-    valid = [p for p in parts if re.match(r'(sd.|nvme.*|hd.|mmcblk.)\d*', p)]
+    # Only keep real disk devices (sd*, nvme*, mmcblk*)
+    real = [p for p in devices if re.match(r'^(sd\w+|nvme\d+n\d+p?\d*|mmcblk\d+p?\d*)$', p)]
+    if fs not in real:
+        cg = subprocess.run("cat /proc/1/cgroup", shell=True, capture_output=True, text=True)
+        return True # Home not on real disk = live session
 
-    if fs_device not in valid:
-        try:
-            cg = subprocess.check_output("cat /proc/1/cgroup", shell=True).decode()
-            if "container" in cg:
-                return False
-        except:
-            pass
-        return True
-
-    return False
+    return False # Home on real disk = installed system
 
 if __name__ == "__main__":
+    system = platform.system()
     app = QApplication([])
 
-    if is_live():
-        # TODO: Show live linux gui
-        # DEBUG: print("Live distro")
-        pass
-    else:
-        # TODO: Show installed linux gui
-        # DEBUG: win = MainApp()
-        # DEBUG: win.show()
-        # DEBUG: app.exec()
-        # DEBUG: print("Installed distro")
-        pass
+    if system == "Windows":
+        win = MainApp()
+
+    elif system == "Linux":
+        if is_live():
+            # TODO: Show live linux gui
+            # DEBUG: print("Live distro")
+            pass
+        else:
+            # TODO: Show installed linux gui
+            # DEBUG: win = MainApp()
+            # DEBUG: win.show()
+            # DEBUG: app.exec()
+            # DEBUG: print("Installed distro")
+            pass
