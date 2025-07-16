a6aa28581e85734063b7d4d6d7308ea219abbddc
Raffa <raffaelegual@tutamail.com>
2025-07-16 19:56:28 +0200

    Add GPL license header to all source files

--- gui/image_rc.py
+++ gui/image_rc.py
@@ -3,6 +3,22 @@
 # Created by: The Resource Compiler for Qt version 6.9.1
 # WARNING! All changes made in this file will be lost!
 
+# Windows to Linux migrator - helps to switch to a linux distro easily
+# Copyright (C) 2025  Raffaele
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from PySide6 import QtCore
 
 qt_resource_data = b"\
